d5446ac33ae8bc2a251dc63bdd3007a0fb97d6b1
小火娃 <1178844902@qq.com>
2022-11-13 15:36:54 +0800

    master test

--- git_test/src/main/java/com/xiaohuowa/Test.java
+++ git_test/src/main/java/com/xiaohuowa/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println("hot-fix here");
+        System.out.println("master test2");
     }
 }
