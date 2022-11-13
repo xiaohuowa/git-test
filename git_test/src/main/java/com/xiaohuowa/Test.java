300cff76f87182ff8c69a0037f6116a9ad6ca28e
小火娃 <1178844902@qq.com>
2022-11-13 15:27:38 +0800

    second commit by IDE

--- git_test/src/main/java/com/xiaohuowa/Test.java
+++ git_test/src/main/java/com/xiaohuowa/Test.java
@@ -8,5 +8,6 @@ package com.xiaohuowa;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
