86d68b54e77402fe43b14ce13b0f4aabce155aa9
小火娃 <1178844902@qq.com>
2022-11-13 15:24:57 +0800

    first commit by IDE

---
+++ git_test/src/main/java/com/xiaohuowa/Test.java
@@ -0,0 +1,12 @@
+package com.xiaohuowa;
+
+/**
+ * @author 小火娃
+ * @project_name: idea_workspace
+ * @package_name: com.xiaohuowa
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
